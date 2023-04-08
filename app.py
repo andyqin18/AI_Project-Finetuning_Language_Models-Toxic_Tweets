797d7d49b6a9484c53ab87ab53030eddf38ecc30
Andy Qin <tianhao.qin@nyu.edu>
2023-04-08 02:41:46 -0400

    Added default model

--- app.py
+++ app.py
@@ -5,10 +5,20 @@ st.title("Sentiment Analysis App - beta")
 st.header("This app is to analyze the sentiments behind a text. Currently it uses \
           pre-trained models without fine-tuning.")
 
-st.text_input("Enter your text:", value="Missing Sophie.Z...")
-st.selectbox("Please select a model:" ("Model 1", "Model 2", "Model 3"))
+user_input = st.text_input("Enter your text:", value="Missing Sophie.Z...")
+st.selectbox("Please select a model:", ("Model 1", "Model 2", "Model 3"))
+
+
 
 if st.button("Analyze"):
-    st.write("You clicked a button.")
+    model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    res = classifier(user_input)
+    st.write(res)
+    
 else:
     st.write("Go on! Try the app!")
