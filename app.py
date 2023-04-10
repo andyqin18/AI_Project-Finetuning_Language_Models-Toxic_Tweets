b6852b8ac41afddb2ebd1b259fe725244fcffde2
Andy Qin <tianhao.qin@nyu.edu>
2023-04-09 23:06:06 -0400

    Finished milestone2

--- app.py
+++ app.py
@@ -1,40 +1,48 @@
 import streamlit as st
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 
-def analyze(model_name, text):
+# Define analyze function
+def analyze(model_name: str, text: str) -> dict:
+    '''
+    Output result of sentiment analysis of a text through a defined model
+    '''
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
     return classifier(text)
 
-st.title("Sentiment Analysis App - beta")
-st.write("This app is to analyze the sentiments behind a text. \n Currently it uses \
-          pre-trained models without fine-tuning.")
+# App title 
+st.title("Sentiment Analysis App - Milestone2")
+st.write("This app is to analyze the sentiments behind a text.")
+st.write("Currently it uses pre-trained models without fine-tuning.")
 
+# Model hub
 model_descrip = {
-    "distilbert-base-uncased-finetuned-sst-2-english": "This model is a fine-tune checkpoint of DistilBERT-base-uncased, fine-tuned on SST-2.\n \
+    "distilbert-base-uncased-finetuned-sst-2-english": "This model is a fine-tune checkpoint of DistilBERT-base-uncased, fine-tuned on SST-2. \
         Labels: POSITIVE; NEGATIVE ",
-    "cardiffnlp/twitter-roberta-base-sentiment": "This is a roBERTa-base model trained on ~58M tweets and finetuned for sentiment analysis with the TweetEval benchmark.\n \
+    "cardiffnlp/twitter-roberta-base-sentiment": "This is a roBERTa-base model trained on ~58M tweets and finetuned for sentiment analysis with the TweetEval benchmark. \
         Labels: 0 -> Negative; 1 -> Neutral; 2 -> Positive",
-    "finiteautomata/bertweet-base-sentiment-analysis": "Model trained with SemEval 2017 corpus (around ~40k tweets). Base model is BERTweet, a RoBERTa model trained on English tweets. \n \
+    "finiteautomata/bertweet-base-sentiment-analysis": "Model trained with SemEval 2017 corpus (around ~40k tweets). Base model is BERTweet, a RoBERTa model trained on English tweets.  \
         Labels: POS; NEU; NEG"
 }
 
+user_input = st.text_input("Enter your text:", value="NYU is the better than Columbia.")
+user_model = st.selectbox("Please select a model:", model_descrip)
 
-user_input = st.text_input("Enter your text:", value="Missing Sophie.Z...")
-user_model = st.selectbox("Please select a model:", 
-                          model_descrip)
-
+# Display model information
 st.write("### Model Description:")
 st.write(model_descrip[user_model])
 
-
+# Perform analysis and print result
 if st.button("Analyze"):
     if not user_input:
         st.write("Please enter a text.")
     else:
         with st.spinner("Hang on.... Analyzing..."):
             result = analyze(user_model, user_input)
-            st.write(f"Result: \nLabel: {result[0]['label']} Score: {result[0]['score']}")
+            st.write("Result:")
+            st.write(f"Label: **{result[0]['label']}**")
+            st.write(f"Confidence Score: **{result[0]['score']}**")
+
 else:
     st.write("Go on! Try the app!")
