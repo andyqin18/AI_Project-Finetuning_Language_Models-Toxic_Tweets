aa2cadbba008eb21908e33f8e351cef4a974a194
Andy Qin <tianhao.qin@nyu.edu>
2023-04-28 00:09:28 -0400

    Debug

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 
-
+# Define global variables
 fine_tuned_model = "andyqin18/test-finetuned"
 sample_text_num = 10
 
@@ -34,17 +34,13 @@ model_descrip = {
         Labels: POS; NEU; NEG"
 }
 
-
-
-
 user_input = st.text_input("Enter your text:", value="NYU is the better than Columbia.")
 user_model = st.selectbox("Please select a model:", model_descrip)
+
 
 # Display model information
 st.write("### Model Description:")
 st.write(model_descrip[user_model])
-
-
 
 
 # Perform analysis and print result
@@ -53,8 +49,9 @@ if st.button("Analyze"):
         st.write("Please enter a text.")
     else:
         with st.spinner("Hang on.... Analyzing..."):
+            # If fine-tuned
             if user_model == fine_tuned_model:
-                result = analyze(user_model, user_input, top_k=2)
+                result = analyze(user_model, user_input, top_k=2)  # Top 2 labels with highest score
                 result_dict = {
                         "Text": [user_input],
                         "Highest Toxicity Class": [result[0][0]['label']],
@@ -63,29 +60,32 @@ if st.button("Analyze"):
                         "Second Highest Score": [result[0][1]['score']]
                             }
                 st.dataframe(pd.DataFrame(result_dict))
+
+                # 10 Sample Table
                 if st.button("Click to generate ten sample analysis"):
-                    df = pd.read_csv("milestone3/comp/test_comment.csv")
-                    test_texts = df["comment_text"].values
-                    sample_texts = np.random.choice(test_texts, size=sample_text_num, replace=False)
+                    with st.spinner("Hang on.... Analyzing..."):
+                        df = pd.read_csv("milestone3/comp/test_comment.csv")
+                        test_texts = df["comment_text"].values
+                        sample_texts = np.random.choice(test_texts, size=sample_text_num, replace=False)
 
-                    init_table_dict = {
-                                "Text": [],
-                                "Highest Toxicity Class": [],
-                                "Highest Score": [],
-                                "Second Highest Toxicity Class": [],
-                                "Second Highest Score": []
-                                    }
+                        init_table_dict = {
+                                    "Text": [],
+                                    "Highest Toxicity Class": [],
+                                    "Highest Score": [],
+                                    "Second Highest Toxicity Class": [],
+                                    "Second Highest Score": []
+                                        }
 
-                    for text in sample_texts:
-                        result = analyze(fine_tuned_model, text[:50], top_k=2)
-                        init_table_dict["Text"].append(text[:50])
-                        init_table_dict["Highest Toxicity Class"].append(result[0][0]['label'])
-                        init_table_dict["Highest Score"].append(result[0][0]['score'])
-                        init_table_dict["Second Highest Toxicity Class"].append(result[0][1]['label'])
-                        init_table_dict["Second Highest Score"].append(result[0][1]['score'])
+                        for text in sample_texts:
+                            result = analyze(fine_tuned_model, text[:50], top_k=2)
+                            init_table_dict["Text"].append(text[:50])
+                            init_table_dict["Highest Toxicity Class"].append(result[0][0]['label'])
+                            init_table_dict["Highest Score"].append(result[0][0]['score'])
+                            init_table_dict["Second Highest Toxicity Class"].append(result[0][1]['label'])
+                            init_table_dict["Second Highest Score"].append(result[0][1]['score'])
                         st.dataframe(pd.DataFrame(init_table_dict))
                 else:
-                    st.write("(─‿‿─)")
+                    st.write("( ─ ‿ ‿ ─ )")
 
 
             else:
